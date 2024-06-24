72755c9ae6b1d3601e90cd3d7a60828156f49a67
ajaypanthagani <ajay.panthagani@ninjavan.co>
2024-06-25 01:36:46 +0530

    feat: add robust exception handling in data formatter

--- src/util/data.cpp
+++ src/util/data.cpp
@@ -3,10 +3,14 @@
 namespace Util {
     dbms::Data formatRawDataToDBData(const dbms::Table &table, const std::string &colName, const std::string &data) {
         auto schema = table.schema();
-        auto colType = schema[colName];
+        auto it = schema.find(colName);
+        if (it == schema.end()) {
+            throw std::runtime_error("Column not found in schema: " + colName);
+        }
+        auto colType = it->second;
 
         dbms::Data dbData;
-        try{
+        try {
             if (colType == Constants::DataType::INT) {
                 dbData.set_int_value(std::stoi(data));
             } else if (colType == Constants::DataType::DOUBLE) {
@@ -16,11 +20,14 @@ namespace Util {
             } else if (colType == Constants::DataType::BOOL) {
                 dbData.set_bool_value(data == "true");
             } else {
-                throw std::runtime_error("unknown data type " + colType);
+                throw std::runtime_error("Unknown data type: " + colType);
             }
-        } catch(const std::exception& e){
-            std::cerr << e.what() << std::endl;
-            throw std::runtime_error("exception when setting value " + data + " for " + colName + "of type " + colType);
+        } catch (const std::invalid_argument& e) {
+            throw std::runtime_error("Invalid argument `" + data + "` for `" + colName + "` of type `" + colType + "`");
+        } catch (const std::out_of_range& e) {
+            throw std::runtime_error("Data size out of range when setting value `" + data + "` for `" + colName + "` of type `" + colType + "`");
+        } catch (const std::exception& e) {
+            throw std::runtime_error("Exception when setting value `" + data + "` for `" + colName + "` of type `" + colType + "`");
         }
 
         return dbData;
